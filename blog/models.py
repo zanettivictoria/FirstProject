374ce46a15b4cdd2ab5dbf8c38efd241d04d8bcd
zanettivictoria <victoria.zanetti21@gmail.com>
2017-11-11 13:47:33 -0200

    Meu Django a PP, first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    criar_data = models.DateTimeField(default=timezone.now)
+    publicar_data = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publicar_data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
